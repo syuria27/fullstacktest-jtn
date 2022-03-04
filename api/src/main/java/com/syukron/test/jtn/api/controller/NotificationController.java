a2d944d6f8b1d1d31832421f88629267c1d7d63a
Syukron Rizqi Ardhi <syukron.tkj2@gmail.com>
2022-03-04 23:47:47 +0700

    add endpoint websocket

---
+++ api/src/main/java/com/syukron/test/jtn/api/controller/NotificationController.java
@@ -0,0 +1,25 @@
+package com.syukron.test.jtn.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * @author Syukron R. Ardhi
+ * @created 04/03/22
+ */
+@Slf4j
+@CrossOrigin
+@Controller
+public class NotificationController {
+
+    @MessageMapping("/notification")
+    @SendTo("/topic/notification")
+    public String greeting(String message) throws Exception {
+        log.info("Notification in = "+message);
+        Thread.sleep(2000);
+        return "Notification "+message;
+    }
+}
